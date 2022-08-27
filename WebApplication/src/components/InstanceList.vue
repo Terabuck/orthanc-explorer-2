9c19af6c99302b01997dd9b244a2d9abe9e0e913
Ludwig <ludwig.moreno@gmail.com>
2022-08-27 14:29:38 +0000

    modified:   src/components/InstanceList.vue 	modified:   src/components/SeriesList.vue

--- WebApplication/src/components/InstanceList.vue
+++ WebApplication/src/components/InstanceList.vue
@@ -1,102 +1,91 @@
 <script>
-import InstanceItem from "./InstanceItem.vue";
-import api from "../orthancApi";
+import InstanceItem from "./InstanceItem.vue"
+import api from "../orthancApi"
 
 export default {
-  props: ["seriesId"],
-  emits: ["deletedInstance"],
-  data() {
-    return {
-      loaded: false,
-      instancesInfo: {},
-    };
-  },
-  computed: {
-    sortedInstancesIds() {
-      if (this.loaded) {
-        let keys = Object.keys(this.instancesInfo);
-        keys.sort((a, b) =>
-          parseInt(this.instancesInfo[a].IndexInSeries) >
-          parseInt(this.instancesInfo[b].IndexInSeries)
-            ? 1
-            : -1
-        );
-        return keys;
-      } else {
-        return [];
-      }
+    props: ['seriesId'],
+    emits: ['deletedInstance'],
+    data() {
+        return {
+            loaded: false,
+            instancesInfo: {}
+        };
     },
-  },
-  async mounted() {
-    const response = await api.getSeriesInstances(this.seriesId);
-    for (const instanceInfo of response.data) {
-      this.instancesInfo[instanceInfo.ID] = instanceInfo;
-    }
-    this.loaded = true;
-  },
-  methods: {
-    onDeletedInstance(instanceId) {
-      delete this.instancesInfo[instanceId];
-      this.$emit("deletedInstance", instanceId);
+    computed: {
+        sortedInstancesIds() {
+            if (this.loaded) {
+                let keys = Object.keys(this.instancesInfo);
+                keys.sort((a, b) => (parseInt(this.instancesInfo[a].IndexInSeries) > parseInt(this.instancesInfo[b].IndexInSeries) ? 1 : -1))
+                return keys;
+            } else {
+                return [];
+            }
+        }
     },
-  },
-  components: { InstanceItem },
-};
+    async mounted() {
+        const response = await api.getSeriesInstances(this.seriesId)
+        for (const instanceInfo of response.data) {
+            this.instancesInfo[instanceInfo.ID] = instanceInfo;
+        }
+        this.loaded = true;
+    },
+    methods: {
+        onDeletedInstance(instanceId) {
+            delete this.instancesInfo[instanceId];
+            this.$emit("deletedInstance", instanceId);
+        }
+    },
+    components: { InstanceItem }
+}
 </script>
 
 <template>
-  <table class="table table-responsive table-sm instance-table">
-    <thead>
-      <th width="2%" scope="col" class="instance-table-header"></th>
-      <th
-        width="7%"
-        scope="col"
-        class="instance-table-header cut-text"
-        data-bs-toggle="tooltip"
-        title="Instance Number"
-      >
-        Instance Number
-      </th>
-      <th
-        width="40%"
-        scope="col"
-        class="instance-table-header cut-text"
-        data-bs-toggle="tooltip"
-        title="SOP Instance UID"
-      >
-        SOP Instance UID
-      </th>
-      <th
-        width="5%"
-        scope="col"
-        class="series-table-header cut-text text-center"
-        data-bs-toggle="tooltip"
-        title="Number of Frames"
-      >
-        # frames
-      </th>
-    </thead>
-    <InstanceItem
-      v-for="instanceId in sortedInstancesIds"
-      :key="instanceId"
-      :instanceId="instanceId"
-      :instanceInfo="instancesInfo[instanceId]"
-      @deletedInstance="onDeletedInstance"
-    ></InstanceItem>
-  </table>
+    <table class="table table-responsive table-sm instance-table">
+        <thead>
+            <th width="2%" scope="col" class="instance-table-header"></th>
+            <th
+                width="7%"
+                scope="col"
+                class="instance-table-header cut-text"
+                data-bs-toggle="tooltip"
+                title="Instance Number"
+            >Instance Number</th>
+            <th
+                width="40%"
+                scope="col"
+                class="instance-table-header cut-text"
+                data-bs-toggle="tooltip"
+                title="SOP Instance UID"
+            >SOP Instance UID</th>
+            <th
+                width="5%"
+                scope="col"
+                class="series-table-header cut-text text-center"
+                data-bs-toggle="tooltip"
+                title="Number of Frames"
+            ># frames</th>
+        </thead>
+        <InstanceItem
+            v-for="instanceId in sortedInstancesIds"
+            :key="instanceId"
+            :instanceId="instanceId"
+            :instanceInfo="instancesInfo[instanceId]"
+            @deletedInstance="onDeletedInstance"
+        ></InstanceItem>
+    </table>
 </template>
 
 <style>
 .instance-table > :not(:first-child) {
-  border-top: 0px !important;
-  color: #acdbcc;
+    border-top: 0px !important;
+    color: #acdbcc;
 }
 
 .instance-table > :first-child {
-  border-bottom: 2px !important;
-  border-style: solid !important;
-  border-color: black !important;
-  color: #89bdac;
+    border-bottom: 2px !important;
+    border-style: solid !important;
+    border-color: black !important;
+    color: #89bdac;
 }
 
 .instance-table > :nth-child(odd) > * {
@@ -110,12 +99,12 @@ export default {
 }
 
 .instance-table-header {
-  text-align: left;
-  padding-left: 10px;
+    text-align: left;
+    padding-left: 10px;
 }
 
 .instance-table td {
-  text-align: left;
-  padding-left: 10px;
+    text-align: left;
+    padding-left: 10px;
 }
 </style>
