d227a42c7b015aeaa326241c1e6212ce299d5880
Ludwig <ludwig.moreno@gmail.com>
2022-09-24 11:39:36 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/assets/css/_variables.css 	modified:   src/components/SideBar.vue 	deleted:    src/components/common/Dropdown.vue 	modified:   src/locales/en.json 	modified:   src/locales/es.json 	modified:   src/locales/i18n.js 	modified:   src/main.js 	modified:   ../scripts/nginx-dev.conf 	src/components/Dropdown.vue 	src/locales/fr.json 	src/locales/gr.json 	src/locales/it.json 	src/locales/jp.json 	src/locales/nl.json 	src/locales/pt.json 	src/locales/ru.json 	src/locales/zh.json 	src/store/modules/i18.js

--- WebApplication/src/locales/i18n.js
+++ WebApplication/src/locales/i18n.js
@@ -1,11 +1,27 @@
 import { createI18n } from "vue-i18n";
 import en from "./en.json";
 import es from "./es.json";
+import fr from "./fr.json";
+import it from "./it.json";
+import pt from "./pt.json";
+import ru from "./ru.json";
+import gr from "./gr.json";
+import nl from "./nl.json";
+import jp from "./jp.json";
+import zh from "./zh.json";
 const i18n = createI18n({
-  locale: "en",
+  locale: "es",
   messages: {
     en,
     es,
+    fr,
+    it,
+    pt,
+    ru,
+    gr,
+    nl,
+    jp,
+    zh,
   },
 });
 
