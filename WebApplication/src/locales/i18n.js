dd58eba67d38590a2080c90c64d695caa0c91cea
Ludwig <ludwig.moreno@gmail.com>
2022-09-25 18:49:19 +0000

    new file:   src/locales/bn.json 	renamed:    src/locales/gr.json -> src/locales/de.json 	modified:   src/locales/en.json 	modified:   src/locales/es.json 	modified:   src/locales/fr.json 	new file:   src/locales/hi.json 	modified:   src/locales/i18n.js 	new file:   src/locales/in.json 	modified:   src/locales/it.json 	modified:   src/locales/jp.json 	modified:   src/locales/nl.json 	modified:   src/locales/pt.json 	modified:   src/locales/ru.json 	new file:   src/locales/sw.json 	modified:   src/locales/zh.json 	modified:   src/store/modules/i18.js

--- WebApplication/src/locales/i18n.js
+++ WebApplication/src/locales/i18n.js
@@ -5,9 +5,12 @@ import fr from "./fr.json";
 import it from "./it.json";
 import pt from "./pt.json";
 import ru from "./ru.json";
-import gr from "./gr.json";
+import de from "./de.json";
 import nl from "./nl.json";
 import jp from "./jp.json";
+import bn from "./bn.json";
+import in from "./in.json";
+import hi from "./hi.json";
 import zh from "./zh.json";
 const i18n = createI18n({
   locale: "en",
@@ -18,9 +21,13 @@ const i18n = createI18n({
     it,
     pt,
     ru,
-    gr,
+    de,
     nl,
     jp,
+    in,
+    bn,
+    hi,
+    sw,
     zh,
   },
 });
