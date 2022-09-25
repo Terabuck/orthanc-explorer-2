be37ab7036042496583aae08a42bcff3aac1062a
Ludwig <ludwig.moreno@gmail.com>
2022-09-25 16:22:03 +0000

    new file:   WebApplication/src/components/Dropdown.vue 	new file:   WebApplication/src/locales/fr.json 	new file:   WebApplication/src/locales/gr.json 	new file:   WebApplication/src/locales/it.json 	new file:   WebApplication/src/locales/jp.json 	new file:   WebApplication/src/locales/nl.json 	new file:   WebApplication/src/locales/pt.json 	new file:   WebApplication/src/locales/ru.json 	new file:   WebApplication/src/locales/zh.json 	new file:   WebApplication/src/store/modules/i18.js

---
+++ WebApplication/src/components/Dropdown.vue
@@ -0,0 +1,65 @@
+<script>
+import langs from '../store/modules/i18';
+export default {
+  name: 'App',
+  data() {
+    return {
+      langs,
+      option: ''
+    };
+
+  },
+  created() {
+    this.option = 'English'
+  },
+  methods: {
+    changeLanguage(key, name) {
+      this.$i18n.locale = key;
+      this.option = name
+    }
+  },
+
+
+}
+</script>
+    
+<template>
+  <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+      aria-expanded="false">
+      <i class="bi bi-translate"></i> {{option}}
+    </button>
+    <ul class="dropdown-menu">
+      <li v-for="(lang, i) in langs" :key="`lang-${i}`" :value="lang"><a class="dropdown-item" href="#"
+          @click="changeLanguage(`${lang.abbreviation}`,`${lang.name}`)">{{lang.name}}</a></li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.btn-primary {
+    --bs-btn-color: #1c411c;
+    --bs-btn-bg: #3f5c3f;
+    --bs-btn-border-color: #5ba05a;
+    --bs-btn-hover-color: #77d375;
+    --bs-btn-hover-bg: #3a633a;
+    --bs-btn-hover-border-color: #264126;
+    --bs-btn-focus-shadow-rgb: #a3b5d1;
+    --bs-btn-active-color: #fff;
+    --bs-btn-active-bg: #559255;
+    --bs-btn-active-border-color: #559255;
+    --bs-btn-active-shadow: inset 0 3px 5pxrgba(0, 0, 0, 0.125);
+    --bs-btn-disabled-color: #fff;
+    --bs-btn-disabled-bg: #559255;
+    --bs-btn-disabled-border-color: #559255;
+}
+.dropdown{
+    margin-top: var(--details-top-margin);
+}
+.dropdown-menu {
+    margin-top: var(--details-top-margin);
+    background-color: var(--study-selected-color);
+    font-size: 0.9rem;
+}
+</style>
+    
---
+++ WebApplication/src/store/modules/i18.js
@@ -0,0 +1,14 @@
+
+let langs = [
+    { name: 'Español', abbreviation: 'es' },
+    { name: 'Française', abbreviation: 'fr' },
+    { name: 'Italiano', abbreviation: 'it' },
+    { name: 'Русский', abbreviation: 'ru' },
+    { name: 'Deutsche', abbreviation: 'gr' },
+    { name: 'Nederlands', abbreviation: 'nl' },
+    { name: 'Português', abbreviation: 'pt' },
+    { name: '日本語', abbreviation: 'jp' },
+    { name: '中文', abbreviation: 'zh' },
+    { name: 'English', abbreviation: 'en' }
+]
+export default langs;
