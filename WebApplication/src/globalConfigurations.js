4c389b15b25cca4a0c473d7fbb336e25c987fd08
anydayr <annyrondon491@gmail.com>
2022-09-27 06:55:20 +0000

    delete console.log

--- WebApplication/src/globalConfigurations.js
+++ WebApplication/src/globalConfigurations.js
@@ -3,4 +3,3 @@ export var baseOe2Url = window.location.pathname.slice(0, -1);
 export var orthancApiUrl = '../../'
 export var oe2ApiUrl = '../api/';
 export var localeConf = DefaultConfiguration.OrthancExplorer2.UiOptions.DefaultLocale;
-console.log(DefaultConfiguration.OrthancExplorer2.UiOptions.DefaultLocale,"ajaa")
