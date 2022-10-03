c76cb5ab83f84fd17196d73c4ebb3edcb65a120e
anydayr <annyrondon491@gmail.com>
2022-10-03 02:34:02 +0000

    modal

--- WebApplication/src/globalConfigurations.js
+++ WebApplication/src/globalConfigurations.js
@@ -1,5 +1,4 @@
-import DefaultConfiguration from "../../Plugin/DefaultConfiguration.json"
 export var baseOe2Url = window.location.pathname.slice(0, -1);
 export var orthancApiUrl = '../../'
 export var oe2ApiUrl = '../api/';
-export var localeConf = DefaultConfiguration.OrthancExplorer2.UiOptions.DefaultLocale;
+export var localeConf ='es';
